d72cf4d79a9364cad9807d3084ebe990ed367ec7
shichenchong <shichenchong@inspur.com>
2020-06-20 14:39:08 +0800

    创建工程

---
+++ phonerecommend/src/main/java/com.sceon/phonerecommend/PhonerecommendApplication.java
@@ -0,0 +1,13 @@
+package phonerecommend.phonerecommend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhonerecommendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PhonerecommendApplication.class, args);
+    }
+
+}
---
+++ phonerecommend/src/test/java/phonerecommend/phonerecommend/PhonerecommendApplicationTests.java
@@ -0,0 +1,13 @@
+package phonerecommend.phonerecommend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhonerecommendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
